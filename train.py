c790a3cd81c2e5c85901659a70f599acdfb1338d
Ray Phan <ray@hover.to>
2019-08-18 21:09:55 +0000

    Using Xception with more data augmentation - best model so far

--- train.py
+++ train.py
@@ -1,13 +1,13 @@
 from keras.utils import multi_gpu_model
-from keras.applications.resnet50 import ResNet50, preprocess_input
+from keras.applications.xception import Xception
 from keras.models import Model, Sequential
-from keras.layers import Input, Lambda, BatchNormalization, Dense, Conv2D, MaxPooling2D, Flatten, Dropout
+from keras.layers import Input, Lambda, BatchNormalization, Dense, Conv2D, MaxPooling2D, Flatten, Dropout, GlobalAveragePooling2D, GlobalMaxPooling2D
 from keras.callbacks import ModelCheckpoint
 from keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 
 class Train:
-    def __init__(self, train_segment_dir, val_segment_dir, batch_size=32, dims=(256, 256)):
+    def __init__(self, train_segment_dir, val_segment_dir, batch_size=32, dims=(224, 224)):
         self.train_segment_dir = train_segment_dir
         self.val_segment_dir = val_segment_dir
         self.img_width = dims[1]
@@ -17,12 +17,15 @@ class Train:
         print("Preparing to load images and the assigned classes...")
         train_datagen = ImageDataGenerator(
             rescale=1.0 / 255.0,
-            shear_range=0.2,
-            zoom_range=0.2,
+            rotation_range=15,
+            shear_range=0.01,
             height_shift_range=0.1,
             width_shift_range=0.1,
+            zoom_range=[0.9, 1.25],
             vertical_flip=False,
-            horizontal_flip=True)
+            horizontal_flip=True,
+            brightness_range=[0.5,1.5],
+            fill_mode='nearest')
         val_datagen = ImageDataGenerator(
             rescale=1.0 / 255.0)
 
@@ -40,52 +43,32 @@ class Train:
             class_mode='categorical')
 
         print("Build the model...")
-        #model = Sequential()
-        #model.add(Conv2D(32, (3, 3), padding="same",
-        #    input_shape=(self.img_height, self.img_width, 3), activation="relu"))
-        #model.add(Conv2D(32, (3, 3), padding="same", activation="relu"))
-        #model.add(BatchNormalization())
-        #model.add(MaxPooling2D(pool_size=(2, 2)))
-        #
-        #model.add(Conv2D(64, (3, 3), padding="same", activation="relu"))
-        #model.add(Conv2D(64, (3, 3), padding="same", activation="relu"))
-        #model.add(BatchNormalization())
-        #model.add(MaxPooling2D(pool_size=(2, 2)))
-        #
-        #model.add(Conv2D(128, (3, 3), padding="same", activation="relu"))
-        #model.add(Conv2D(128, (3, 3), padding="same", activation="relu"))
-        #model.add(BatchNormalization())
-        #model.add(MaxPooling2D(pool_size=(2, 2)))
-        #
-        #model.add(Conv2D(256, (3, 3), padding="same", activation="relu"))
-        #model.add(Conv2D(256, (3, 3), padding="same", activation="relu"))
-        #model.add(BatchNormalization())
-        #model.add(MaxPooling2D(pool_size=(2, 2)))
-        #
-        #model.add(Flatten())
-        #model.add(Dense(512, activation='relu'))
-        #model.add(Dropout(0.5))
-        #model.add(Dense(512, activation='relu'))
-        #model.add(Dropout(0.5))
-        #model.add(Dense(10, activation='softmax'))
 
         # Use transfer learning from ResNet50 instead - discard Dense layers
-        model = ResNet50(weights='imagenet', input_shape=(self.img_width, self.img_height, 3),
+        model = Xception(weights='imagenet', input_shape=(self.img_width, self.img_height, 3),
                          include_top=False)
         # Make sure to preprocess input that ResNet50 is expecting
         #inp = Input(shape=(self.img_width, self.img_height, 3))
-        inp = model.input
         #x = Lambda(lambda x: preprocess_input(x))
+
+        inp = model.input
         
         # Create new dense layers on top
-        #x = Flatten()(model(x))
         x = model.output
-        x = Flatten()(x)
-        x = Dense(256, activation='relu')(x)
+        x = GlobalAveragePooling2D()(x)
+        x = Dense(1024, activation='relu')(x)
         x = Dropout(0.5)(x)
-        x = Dense(256, activation='relu')(x)
+        x = Dense(1024, activation='relu')(x)
         predictions = Dense(10, activation='softmax')(x)
         final_model = Model(input=inp, outputs=predictions)
+
+        # Only set batchnorm, dense and softmax layer to trainable
+        #for layer in final_model.layers:
+        #    s = layer.name.lower()
+        #    layer.trainable = False
+        #    if 'bn' in s or 'dense' in s or 'softmax' in s:
+        #        layer.trainable = True
+
         final_gpu_model = multi_gpu_model(final_model, gpus=4) # New - training over multiple GPUs
         filepath = 'model_bodyfat.hdf5'
         checkpoint = ModelCheckpoint(filepath,
@@ -109,7 +92,7 @@ class Train:
             max_queue_size=10,
             workers=64,
             shuffle=False,
-            epochs=100)
+            epochs=250)
 
         acc = history.history['acc']
         val_acc = history.history['val_acc']
@@ -124,5 +107,5 @@ class Train:
 
 train = Train(train_segment_dir='/home/ubuntu/travis/ImageProcessing/output_seg/train_segment',
               val_segment_dir='/home/ubuntu/travis/ImageProcessing/output_seg/val_segment',
-              batch_size=8, dims=(512, 512))
+              batch_size=16, dims=(511, 511))
 train.build_model()
